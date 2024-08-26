b10d55ab55276a0176e673dd5a15a59c5333ac48
Saim Saqib <saimmoin64@gmail.com>
2024-08-26 15:48:40 +0500

    setLikes updated

--- controllers/likesController.js
+++ controllers/likesController.js
@@ -1,6 +1,7 @@
 /** @format */
 
 const Likes = require("../models/likesModel.js");
+const Post = require("../models/copyModel.js");
 
 const getLikes = async (req, res) => {
   try {
@@ -13,4 +14,39 @@ const getLikes = async (req, res) => {
   }
 };
 
-module.exports = { getLikes };
+const setLikes = async (req, res) => {
+  try {
+    /**
+     * find likes entry by post id
+     * if (likes) entry exits get user index
+     * if user like exits return same object
+     * if not add user address into the array
+     * if entry does not exits ADD entry in database
+     */
+    const { postId, userAddress } = req.body;
+
+    const likesEntry = await Likes.findOne({ postId });
+    console.log(likesEntry);
+
+    if (likesEntry) {
+      let userIndex = likesEntry.likes.indexOf(userAddress);
+      if (userIndex > -1) {
+        res.status(200).json({ data: likesEntry });
+      } else {
+        likesEntry.likes.push(userAddress);
+        const newLikesSave = likesEntry.save();
+        res.status(200).json({ data: newLikesSave });
+      }
+    } else {
+      const newLikes = await Likes.create({
+        postId: postId,
+        likes: [userAddress],
+      });
+      res.status(200).json({ data: newLikes });
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+module.exports = { getLikes, setLikes };
